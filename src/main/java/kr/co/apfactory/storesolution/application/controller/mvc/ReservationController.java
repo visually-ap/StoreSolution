8c92c9469555f05d984e6e4920c7d490ef7c90b7
mwjung <ooohitooo@bzu.co.kr>
2025-07-14 03:50:56 +0900

    - 일부 수정

---
+++ src/main/java/kr/co/apfactory/storesolution/application/controller/mvc/ReservationController.java
@@ -0,0 +1,36 @@
+package kr.co.apfactory.storesolution.application.controller.mvc;
+
+import kr.co.apfactory.storesolution.application.service.SiteService;
+import kr.co.apfactory.storesolution.application.service.StoreService;
+import kr.co.apfactory.storesolution.application.service.UserService;
+import kr.co.apfactory.storesolution.domain.dto.common.SearchDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Log4j2
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/reservation")
+public class ReservationController {
+    private final StoreService storeService;
+
+    private final UserService userService;
+
+    private final SiteService siteService;
+
+    @GetMapping("/main")
+    public String gotoStoreInfoPage(Model model) {
+        model.addAttribute("item", storeService.getStoreInfo());
+        return "views/reservation/main";
+    }
+}

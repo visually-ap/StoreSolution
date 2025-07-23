eb464ca7b4d8b2aff376ab92c5d67232f719554f
mwjung <ooohitooo@bzu.co.kr>
2025-07-23 22:11:21 +0900

    - 카운셀링 일부 세부 페이지 링킹

---
+++ src/main/java/kr/co/apfactory/storesolution/application/controller/mvc/CounselingController.java
@@ -0,0 +1,34 @@
+package kr.co.apfactory.storesolution.application.controller.mvc;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Log4j2
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/counseling")
+public class CounselingController {
+    @GetMapping("/customer/list")
+    public String gotoCustomerList(Model model) {
+        return "views/counseling/customer/list";
+    }
+
+    @GetMapping("/customer/register")
+    public String gotoCustomerRegisterForm(Model model) {
+        return "views/counseling/customer/register";
+    }
+
+    @GetMapping("/about")
+    public String gotoAboutPage(Model model) {
+        return "views/counseling/about";
+    }
+
+    @GetMapping("/fabric-item")
+    public String gotoFabricAndItemPage(Model model) {
+        return "views/counseling/fabricItem";
+    }
+}

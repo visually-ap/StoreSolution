31b13b4e1fd409fd2dce95b229cef57ebe0a2423
hs.kim <helloalice90@gmail.com>
2025-10-13 01:43:04 +0900

    -store management 아이콘 이미지 추가 (consulting partner 2개, rental 2개, 총 4개) -전체 검색창 수정 완료 -reservation 미정 체크박스 들어가는 페이지 수정 -textarea 들어가는 페이지 수정 -요청하신 수정사항 중 1,2,3,4,5, 6 수정완료

--- src/main/resources/static/js/common/modal.js
+++ src/main/resources/static/js/common/modal.js
@@ -1,6 +1,6 @@
 // 공통 헤더 검색창
 $(document).ready(function () {
-    $(".search_icon").click(function () {
+    $(".right_area .search_icon").click(function () {
         $(".search_modal_wrap").fadeIn();
     });
 
